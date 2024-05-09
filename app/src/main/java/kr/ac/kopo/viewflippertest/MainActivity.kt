5a2f42fbcbd59305bb6f0d0d82a3431149b6f392
AISW-509-196 <dlekdnd1@naver.com>
2024-05-09 14:23:22 +0900

    flipinterval

--- app/src/main/java/kr/ac/kopo/viewflippertest/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/viewflippertest/MainActivity.kt
@@ -1,15 +1,33 @@
 package kr.ac.kopo.viewflippertest
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ViewFlipper
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnNext : Button
+    lateinit var btnPrev : Button
+    lateinit var vFlip : ViewFlipper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnNext = findViewById<Button>(R.id.btnNext)
+        btnPrev = findViewById<Button>(R.id.btnPrev)
+        vFlip = findViewById<ViewFlipper>(R.id.flipper)
+
+        btnNext.setOnClickListener(btnListner)
+        btnPrev.setOnClickListener(btnListner)
+
+        vFlip.setFlipInterval(2000)
+    }
+
+    val btnListner = View.OnClickListener {
+        when(it.id){
+            R.id.btnNext -> vFlip.stopFlipping()
+            R.id.btnPrev -> vFlip.startFlipping()
+        }
     }
 }
